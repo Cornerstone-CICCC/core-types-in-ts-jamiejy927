9d92ad0f1bcb94178808c76d81fed84e418fa282
jamie <jamie927@gmail.com>
2026-04-14 20:50:00 -0700

    settings and exercise1

--- dist/ex01.js
+++ dist/ex01.js
@@ -1,21 +1,17 @@
+"use strict";
 // Exercise 1: Create a Reservation Object
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // 1. reservation
-const reservation: {
-    customerInformation: string;
-    paymentInformation: string;
-    checkIn: Date;
-    checkOut: Date;
-} = {
+const reservation = {
     customerInformation: "Jamie",
     paymentInformation: "Credit Card",
     checkIn: new Date("2026-04-14"),
     checkOut: new Date("2026-04-20"),
 };
-
 // 2. console.log
 console.log("--- Reservation Object Information ---");
 console.log("Customer Name:", reservation.customerInformation);
 console.log("Payment Method:", reservation.paymentInformation);
 console.log("Check-In Date:", reservation.checkIn.toDateString());
 console.log("Check-Out Date:", reservation.checkOut.toDateString());
+//# sourceMappingURL=ex01.js.map
